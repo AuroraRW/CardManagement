d672bb98393f5a7030adf6642c376fd6e8c0169b
Aurora <aurorawangca@gmail.com>
2023-02-08 12:30:41 -0700

    sql

---
+++ main.sql
@@ -0,0 +1,76 @@
+------------------------Instruction--------------------
+----1. Install sqlite on your computer or create a repl in replit website
+----2. run "sqlite3 cardsDB.db" in shell
+----3. run ".read main.sql"
+---- control D to exit
+-------------------------------------------------------
+
+-- DROP TABLE IF EXISTS cards;
+-- CREATE TABLE cards (
+-- 	card_id INTEGER PRIMARY KEY,
+-- 	name TEXT NOT NULL,
+-- 	description TEXT NOT NULL,
+-- 	imageurl TEXT NOT NULL,
+-- 	level INTEGER NOT NULL,
+--   point INTEGER NOT NULL
+-- );
+
+-- INSERT INTO cards VALUES (0001, 'Mary', 
+--   'Manager', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 11, 2345);
+-- INSERT INTO cards VALUES (0002, 'Lily', 
+--   'Developer', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 7, 1234);
+-- INSERT INTO cards VALUES (0003, 'Lucy', 
+--   'QA', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 9, 4321);
+-- INSERT INTO cards VALUES (0004, 'Mike', 
+--   'Developer', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 15, 2323);
+-- INSERT INTO cards VALUES (0005, 'Tom', 
+--   'Manager', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 2, 3456);
+-- INSERT INTO cards VALUES (0006, 'Jack', 
+--   'QA', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 5, 6543);
+-- INSERT INTO cards VALUES (0007, 'Jason', 
+--   'Manager', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 12, 5767);
+-- INSERT INTO cards VALUES (0008, 'Ann', 
+--   'Developer', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 7, 8756);
+-- INSERT INTO cards VALUES (0009, 'Zoe', 
+--   'Manager', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 7, 2345);
+-- INSERT INTO cards VALUES (0010, 'Judy', 
+--   'Developer', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 9, 1536);
+-- INSERT INTO cards VALUES (0011, 'Hellen', 
+--   'QA', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 4, 5785);
+-- INSERT INTO cards VALUES (0012, 'Victory', 
+--   'QA', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 16, 3333);
+-- INSERT INTO cards VALUES (0013, 'Vicy', 
+--   'Developer', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 17, 2345);
+-- INSERT INTO cards VALUES (0014, 'Wendy', 
+--   'Developer', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 13, 1346);
+-- INSERT INTO cards VALUES (0015, 'Bob', 
+--   'QA', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 15, 8765);
+-- INSERT INTO cards VALUES (0016, 'Cindy', 
+--   'QA', 'https://cdn-icons-png.flaticon.com/512/147/147144.png', 11, 1357);
+
+
+-- --Query 1:
+-- select * from cards;
+
+-- --Query 2:
+-- select * from cards
+-- where point > 2000
+
+-- --Query 3:
+-- select description, count(card_id), avg(level) from cards
+-- where point > 2000
+-- group by description
+
+-- --Query 4:
+-- select description, count(card_id), avg(level) from cards
+-- where point > 2000
+-- group by description
+-- having avg(level)>9
+
+-- --Query 5:
+-- select description, count(card_id), avg(level) from cards
+-- where point > 2000
+-- group by description
+-- having avg(level)>9
+-- order by avg(level) asc
+
---
+++ school.sql
@@ -0,0 +1,87 @@
+------------------------Instruction--------------------
+----1. Install sqlite on your computer or create a repl in replit website
+----2. run "sqlite3 schoolDB.db" in shell
+----3. run ".read school.sql"
+---- control D to exit
+-------------------------------------------------------
+
+-- DROP TABLE IF EXISTS teachers;
+-- CREATE TABLE teachers (
+--   teacher_id INTEGER PRIMARY KEY, 
+--   teacher_name TEXT
+-- );
+
+-- DROP TABLE IF EXISTS courses;
+-- CREATE TABLE courses (
+--   course_id INTEGER PRIMARY KEY,
+--   course_name TEXT NOT NULL, 
+--   teacher_id INTEGER, 
+--   FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)  
+-- );
+
+-- DROP TABLE IF EXISTS students;
+-- CREATE TABLE students (
+--   student_id INTEGER PRIMARY KEY,
+--   student_name TEXT
+-- );
+
+-- DROP TABLE IF EXISTS student_courses;
+-- CREATE TABLE student_courses (
+--   course_id INTEGER, 
+--   student_id INTEGER, 
+--   FOREIGN KEY (course_id) REFERENCES courses(course_id), 
+--   FOREIGN KEY (student_id) REFERENCES students(student_id),   
+--   PRIMARY KEY (course_id, student_id)
+-- );
+
+-- INSERT INTO teachers VALUES (01, 'Mrs Brown');
+-- INSERT INTO teachers VALUES (02, 'Mr Tate');
+-- INSERT INTO teachers VALUES (03, 'Mrs Filestone');
+-- INSERT INTO teachers VALUES (04, 'Mr Zhao');
+
+-- INSERT INTO students VALUES (01, 'Tom');
+-- INSERT INTO students VALUES (02, 'Mary');
+-- INSERT INTO students VALUES (03, 'Lucy');
+-- INSERT INTO students VALUES (04, 'Lily');
+-- INSERT INTO students VALUES (05, 'Mike');
+-- INSERT INTO students VALUES (06, 'Zoe');
+-- INSERT INTO students VALUES (07, 'Nancy');
+
+
+-- INSERT INTO courses VALUES (01, 'Python', 03);
+-- INSERT INTO courses VALUES (02, 'web development', 02);
+-- INSERT INTO courses VALUES (03, 'Scratch', 01);
+
+-- INSERT INTO student_courses VALUES (01, 02);
+-- INSERT INTO student_courses VALUES (01, 03);
+-- INSERT INTO student_courses VALUES (01, 05);
+-- INSERT INTO student_courses VALUES (02, 05);
+-- INSERT INTO student_courses VALUES (02, 06);
+-- INSERT INTO student_courses VALUES (02, 01);
+-- INSERT INTO student_courses VALUES (03, 04);
+
+-- --Query 1:
+-- select * from students;
+-- select * from teachers;
+-- select * from courses;
+
+-- --Query 2:
+-- select s.student_id, s.student_name, count(sc.course_id)
+-- from students s
+-- left join student_courses sc on s.student_id = sc.student_id
+-- GROUP BY s.student_id;
+
+-- --Query 3:
+-- select s.student_name, c.course_name
+-- from students s
+-- right join student_courses sc on s.student_id = sc.student_id
+-- left join courses c on sc.course_id = c.course_id
+
+-- --Query 4:
+-- select s.student_name, c.course_name, t.teacher_name
+-- from students s
+-- right join student_courses sc on s.student_id = sc.student_id
+-- left join courses c on sc.course_id = c.course_id
+-- left join teachers t on t.teacher_id = c.teacher_id
+
+
